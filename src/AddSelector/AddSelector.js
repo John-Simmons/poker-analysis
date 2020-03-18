972bc8ec4714a59b8821105e6a2eb56dafc54e29
John Simmons <jsimmons2705@gmail.com>
2020-03-18 14:01:16 -0400

    Added the ability to add players.

--- src/AddSelector/AddSelector.js
+++ src/AddSelector/AddSelector.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const addSelector = () => {
+const addSelector = (props) => {
     return(
         <div className="AddSelector">
+            <div
+                className="add-btn"
+                onClick={props.addPlayer}>
+                +
+            </div>
+            <h5 className="add-btn-label">
+                {props.children}
+            </h5>
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import AddSelector from "./AddSelector/AddSelector"
 
 class App extends Component {
     state = {
-        ranges: [
+        players: [
             {id: "Hero", range: [], type: "range"},
             {id: "Villian 1", range: [], type: "range"}
         ]
@@ -17,27 +17,38 @@ class App extends Component {
 
     //This function clears the range selected
     clearRangeHandler = (index) => {
-        console.log("calling clearRangeHandler with", index)
-        const newRanges = [...this.state.ranges]
-        newRanges[index].range = [];
-        this.setState({ranges: newRanges});
+        const newPlayers = [...this.state.players]
+        newPlayers[index].range = [];
+        this.setState({players: newPlayers});
     }
 
     //This function add/subtracts from the range based on the user selection
     selectHandHandler = (handId, index) => {
-        const newRanges = [...this.state.ranges]
-        const range = [...this.state.ranges[index].range];
+        const newPlayers = [...this.state.players]
+        const range = [...this.state.players[index].range];
 
         if (range.includes(handId)){
             const handIndex = range.indexOf(handId);
             range.splice(handIndex, 1);
-            newRanges[index].range = range;
-            this.setState({ranges: newRanges});
+            newPlayers[index].range = range;
+            this.setState({players: newPlayers});
         } else {
-            newRanges[index].range.push(handId);
+            newPlayers[index].range.push(handId);
 
-            this.setState({ranges: newRanges});
+            this.setState({players: newPlayers});
         }
+    }
+
+    //Adds a new player to the list
+    addPlayerHandler = () => {
+        const newPlayers = [...this.state.players]
+
+        let newName = "Villian";
+        newName = newName.concat(newPlayers.length.toString());
+
+        newPlayers.push({id:newName, range:[], type:"range"});
+
+        this.setState({players: newPlayers});
     }
 
     //TO-DO: This function handles a input in the textbox
@@ -53,19 +64,26 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="row no-gutters">
-                    {this.state.ranges.map((entity, index) =>{
-                        return <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                <div className="row">
+                    <AddSelector
+                        addPlayer={this.addPlayerHandler}>
+                        Add Player
+                    </AddSelector>
+                </div>
+                <div className="row">
+                    {this.state.players.map((player, index) =>{
+                        return <div
+                            className="col-xs-12 col-sm-6 col-md-6 col-lg-6"
+                            key={player.id}>
                             <RangeSelector
-                                range={entity.range}
-                                entityId={entity.id}
+                                range={player.range}
+                                entityId={player.id}
                                 index={index}
-                                type={entity.type}
+                                type={player.type}
                                 clearRange={() => this.clearRangeHandler(index)}
                                 enterRange={(event) => this.enterRangeHandler(event)}
-                                selectHand={(handId) => this.selectHandHandler(handId, index)}
-                                key={entity.id}>
-                                {entity.id}
+                                selectHand={(handId) => this.selectHandHandler(handId, index)}>
+                                {player.id}
                             </RangeSelector>
                         </div>
                     })}
